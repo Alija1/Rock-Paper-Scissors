1b36f03027dcbcbf69374ff2ddc8a6c7b4916f70
Alija <alijazecevic@gmail.com>
2023-03-01 22:58:25 +0100

    Updated rps.js, playRound function

--- rps.js
+++ rps.js
@@ -1,9 +1,78 @@
+const playerSelection = prompt("Input: Rock, Paper or Scissors");
 
 
-function getComputerChoice (hand)
-{
-    return hand[Math.floor(Math.random()*hand.length)];
+
+function getComputerChoice() {
+    const computerHand = ["Rock", "Paper", "Scissors"];
+    return computerHand [Math.floor(Math.random()*computerHand.length)];
 }
 
-let hand = ["Rock", "Paper", "Scissors"];
-console.log(getComputerChoice(hand))
+
+console.log(getComputerChoice())
+
+const computerSelection = getComputerChoice();
+
+
+
+function playRound (x, y) {
+
+            if (computerSelection === "Rock"){
+                if (playerSelection === "Rock"){
+                    console.log("Both players picked the same hand. Play again!")
+                }
+                
+            }
+
+            else if (computerSelection === "Rock"){
+                if (playerSelection === "Scissors"){
+                   console.log("Rock beats Scissors! You lose!")
+                }
+                
+            }
+
+            else if (computerSelection === "Rock"){
+                if (playerSelection === "Paper"){
+                    console.log("Paper beats Rock! You win!")
+                }
+            }
+
+            else if (computerSelection === "Paper"){
+                if (playerSelection === "Paper"){
+                    console.log("Both players picked the same hand. Play again!")
+                }
+            }
+
+            else if (computerSelection === "Paper"){
+                if (playerSelection === "Rock"){
+                    console.log("Paper beats Rock! You lose!")
+                }
+            }
+
+            else if (computerSelection === "Paper"){
+                if (playerSelection === "Scissors"){
+                    console.log("Scissors beats Paper! You win!")
+                }
+            }
+
+            else if (computerSelection === "Scissors"){
+                if (playerSelection === "Scissors"){
+                    console.log("Both players picked the same hand. Play again!")
+                }
+            }
+
+            else if (computerSelection === "Scissors"){
+                if (playerSelection === "Paper"){
+                    console.log("Scissors beats Paper! You lose!")
+                }
+            }
+
+            else if (computerSelection === "Scissors"){
+                if (playerSelection === "Rock"){
+                console.log("Rock beats Scissors! You win!")
+                }
+            }
+
+}
+
+
+console.log(playRound(computerSelection, playerSelection))
